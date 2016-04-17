7aa586a40d6f3cf94a4028320b5741272ab1a9b1
Debasish <debasishmisra3008@gmail.com>
2016-04-17 21:08:40 +0530

    Demo

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ http.createServer(function(request, response){
 	
 	response.writeHead(200, {"content-Type":"text/plain"});
 	response.write('Hello Debasish, Welcome to Node JS');
+	console.log(server running now);
 	response.end();
 }).listen(8080);
